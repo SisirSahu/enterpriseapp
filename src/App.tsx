5e98fb94e26028c2d3a7ebc18d8a8f1b76cd8246
sisisahu <sisisahu@qti.qualcomm.com>
2022-03-11 12:41:22 +0530

    starter modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
 
 function App() {
   return (
     <div className="App">
-      <Customer firstName={'Sisir'} lastName={'Sahu'} age={0} isLegal={false}></Customer>
+      <Customer
+        firstName={'Sisir'}
+        lastName={'Sahu'}
+        age={0}
+        isLegal={false}
+      ></Customer>
     </div>
   );
 }
